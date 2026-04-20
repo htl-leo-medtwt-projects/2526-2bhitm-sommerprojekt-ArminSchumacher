48b275dcc246f9e9d5dc1cddf94e9ffcf23a5893
Armin Schumacher <a.schumacher@students.htl-leonding.ac.at>
2026-04-20 12:49:10 +0200

    Player Choose code hinzugefügt

---
+++ project/script.js
@@ -0,0 +1,34 @@
+// VARIABLEN
+let startButton = document.getElementById("start");
+let navTree = document.getElementById("nav-tree");
+let logo = document.getElementById("logo");
+let howToPlayButton = document.getElementById("how-to-play");
+let creditsButton = document.getElementById("credits");
+let playerChoose = document.getElementById("playerChoose");
+
+
+// Startbutton funvtion
+function Start() {
+    navTree.style.transition = "transform 2s ease";
+    navTree.style.transform = "translateX(-100vw)";
+
+    startButton.style.transition = "transform 2s ease";
+    startButton.style.transform = "translateX(-100vw)";
+
+    howToPlayButton.style.transition = "transform 2s ease";
+    howToPlayButton.style.transform = "translateX(-100vw)";
+
+    creditsButton.style.transition = "transform 2s ease";
+    creditsButton.style.transform = "translateX(-100vw)";
+
+    // KI - damit die smoothe transformation funktioniert
+    logo.style.animation = "none";
+    logo.style.transform = "rotate(12deg) scale(1)";
+    void logo.offsetWidth;
+    logo.style.transition = "transform 2s ease";
+    logo.style.transform = "translateX(100vw) rotate(12deg) scale(1)";
+
+    playerChoose.style.transition = "opacity 0.5s ease, top 0.9s ease";
+    playerChoose.style.opacity = "1";
+    playerChoose.style.top = "60%";
+}
