0bed19045ed5c4fac32ae6796219e8fb982335d5
Armin Schumacher <a.schumacher@students.htl-leonding.ac.at>
2026-04-22 10:50:50 +0200

    Player Spielfunktion überarbeitet

--- project/script.js
+++ project/script.js
@@ -6,6 +6,7 @@ let howToPlayButton = document.getElementById("how-to-play");
 let creditsButton = document.getElementById("credits");
 let playerChoose = document.getElementById("playerChoose");
 let characterSelection = document.getElementById("character-selection");
+let fog = document.getElementById("fog");
 
 let PLAYER = {
     box: document.getElementById("player"),
@@ -22,7 +23,7 @@ let KEY_EVENTS = {
 };
 
 let GAME_CONFIG = {
-    gameSpeed: 24,
+    gameSpeed: 28,
     characterSpeed: 5
 };
 
@@ -34,17 +35,29 @@ document.onkeyup = keyListenerUp;
 function keyListenerDown(e) {
     if (e.key === "ArrowLeft") {
         KEY_EVENTS.leftArrow = true;
+        KEY_EVENTS.rightArrow = false;
+        KEY_EVENTS.upArrow = false;
+        KEY_EVENTS.downArrow = false;
     }
 
     if (e.key === "ArrowUp") {
+        KEY_EVENTS.leftArrow = false;
+        KEY_EVENTS.rightArrow = false;
         KEY_EVENTS.upArrow = true;
+        KEY_EVENTS.downArrow = false;
     }
 
     if (e.key === "ArrowRight") {
+        KEY_EVENTS.leftArrow = false;
         KEY_EVENTS.rightArrow = true;
+        KEY_EVENTS.upArrow = false;
+        KEY_EVENTS.downArrow = false;
     }
 
     if (e.key === "ArrowDown") {
+        KEY_EVENTS.leftArrow = false;
+        KEY_EVENTS.rightArrow = false;
+        KEY_EVENTS.upArrow = false;
         KEY_EVENTS.downArrow = true;
     }
 }
@@ -69,16 +82,34 @@ function keyListenerUp(e) {
 
 function selectCharacter(characterNumber) {
     PLAYER.spriteImg.src = "./img/sprite.png";
+    fog.style.transition = "opacity 1s ease";
+    fog.style.opacity = "0";
+
+    if (characterNumber === 1) {
+        // Ziege
+        PLAYER.spriteStartRight = 0;
+    }
+
+    if (characterNumber === 2) {
+        // Weißes Schaf
+        PLAYER.spriteStartRight = 150;
+    }
+
+    if (characterNumber === 3) {
+        // Schwarzes Schaf
+        PLAYER.spriteStartRight = 450;
+    }
 
     playerChoose.style.display = "none";
     characterSelection.style.display = "none";
 
     PLAYER.box.style.display = "block";
 
-    PLAYER.box.style.left = "500px";
-    PLAYER.box.style.top = "300px";
+    PLAYER.box.style.left = "650px";
+    PLAYER.box.style.top = "400px";
 
-    PLAYER.spriteImg.style.right = "0px";
+    PLAYER.spriteImgNumber = 0;
+    PLAYER.spriteImg.style.right = PLAYER.spriteStartRight + "px";
     PLAYER.spriteImg.style.top = "-330px";
 
     gameStarted = true;
@@ -88,6 +119,10 @@ function selectCharacter(characterNumber) {
 function setSpriteDirection(direction) {
     if (direction === "down") {
         PLAYER.spriteImg.style.top = "-330px";
+    }
+
+    if (direction === "up") {
+        PLAYER.spriteImg.style.top = "-548px";
     }
 
     if (direction === "left") {
@@ -145,13 +180,13 @@ function animatePlayer() {
         let x = parseFloat(PLAYER.spriteImg.style.right);
 
         if (isNaN(x)) {
-            x = 0;
+            x = PLAYER.spriteStartRight;
         }
 
         x += 50;
         PLAYER.spriteImg.style.right = x + "px";
     } else {
-        PLAYER.spriteImg.style.right = "0px";
+        PLAYER.spriteImg.style.right = PLAYER.spriteStartRight + "px";
         PLAYER.spriteImgNumber = 0;
     }
 }
@@ -174,6 +209,7 @@ function gameLoop() {
     }
 
     if (KEY_EVENTS.upArrow) {
+        setSpriteDirection("up");
         movePlayer(0, -GAME_CONFIG.characterSpeed, 0);
         animatePlayer();
     }
